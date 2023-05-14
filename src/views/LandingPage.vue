565967355e7969607711e05d893a9aa2079db031
unknown <mutungaeric839@gmail.com>
2023-05-14 13:05:04 +0300

    modified landing page

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,9 +1,26 @@
+
 <template>
-  <div class="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
-    <h1 class="text-5xl mb-8 font-bold text-gray-700">Welcome to SpursMatchUp</h1>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="goToLogin">Log In</button>
+  <div class="landing-page min-h-screen flex flex-col justify-center items-center bg-white">
+    <h1 class="text-5xl mb-8 font-bold text-navy-blue">Welcome to SpursMatchUp</h1>
+    <p class="text-xl mb-4 text-red-600">Get ready to experience the thrill of the Champions League final!</p>
+    <p class="text-lg mb-4 text-navy-blue">Join the thousands of passionate Tottenham Hotspur and Arsenal fans from around the world who will be tuning in to watch this historic match.</p>
+    <p class="text-lg mb-4 text-navy-blue">But don't just watch from the sidelines – be a part of the action and purchase your tickets today!</p>
+    <button class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" @click="goToLogin">Log In</button>
   </div>
 </template>
+
+
+
+<style>
+ .landing-page {
+/*   background-image: url('/home1.jpg'); 
+ */  background-size: ;
+  background-position: center;
+  opacity: ;
+} 
+ 
+
+</style> 
 
 <script>
 export default {
