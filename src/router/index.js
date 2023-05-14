8f6575323d6d9f9a81eaa5e63c8b6d0285079ac3
unknown <mutungaeric839@gmail.com>
2023-05-14 10:27:38 +0300

    landing page created

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LandingPage from '../views/LandingPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'landingpage',
+      component: LandingPage
     },
     {
       path: '/about',
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
+    <h1 class="text-5xl mb-8 font-bold text-gray-700">Welcome to SpursMatchUp</h1>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="goToLogin">Log In</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToLogin() {
+      // Use Vue Router to navigate to the login page
+      this.$router.push('/login');
+    }
+  }
+}
+</script>
