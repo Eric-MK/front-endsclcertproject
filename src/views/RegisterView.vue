ffcb6085e38213fb661adc5cc551e2598802dc34
unknown <mutungaeric839@gmail.com>
2023-06-08 20:32:32 +0300

    registration bg color

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -3,9 +3,9 @@
     
  
     <div class="container mx-auto">
-      <h2 class="text-2xl font-bold mb-4 text-indigo-100 mt-10">Registration</h2>
+      <h2 class="text-2xl font-bold mb-4 text-indigo-100 mt-10">Registration :</h2>
 
-      <form @submit.prevent="registerUser" class="max-w-sm mx-auto">
+      <form @submit.prevent="registerUser" class="bg-slate-900	rounded-lg shadow-md p-8 max-w-md mx-auto w-full" >
         <div class="mb-4">
           <label for="name" class="text-lg font-medium text-blue-900">Name:</label>
           <input type="text" id="name" v-model="formData.name" required class="w-full mt-1 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-400" />
@@ -22,10 +22,6 @@
           <label for="password_confirmation" class="text-lg font-medium text-blue-900">Confirm Password:</label>
           <input type="password" id="password_confirmation" v-model="formData.password_confirmation" required class="w-full mt-1 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-400" />
         </div>
-        <!-- <div class="mb-4">
-          <label for="profile_image" class="text-sm font-medium">Profile Image:</label>
-          <input type="file" id="profile_image" @change="onFileChange" accept="image/*" class="mt-1" />
-        </div> -->
         <button type="submit" class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Register</button>
       </form>
     </div>
@@ -40,6 +36,13 @@ background-image: url('/home2.jpg');
  */  background-position: center;
   background-attachment: fixed;
 } 
+
+.form-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 </style>
   <script>
 
